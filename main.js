2beeae4b0077774c671291f833ca9e6b886ef3fe
Vaibhav kohli <vaibhav.khl@gmail.com>
2014-06-20 12:38:04 +0530

    added module

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function SimpleCtrl($scope) {
+var demoApp = angular.module('demoApp', []);
+
+demoApp.controller('SimpleCtrl', function ($scope) {
 	$scope.persons = [
 	  {name: 'vaibhav', city: 'katni'},
 	  {name: 'mansi', city: 'mumbai'},
 	  {name: 'nikita', city: 'bilaspur'}
 	];
-}
+});
