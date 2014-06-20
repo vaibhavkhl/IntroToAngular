6b7b3bd792847f12752c1c0420e2ab170ecd75e1
Vaibhav kohli <vaibhav.khl@gmail.com>
2014-06-20 13:03:58 +0530

    added routing

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 var demoApp = angular.module('demoApp', []);
 
+demoApp.config(function ($routeProvider) {
+	$routeProvider
+	    .when('/',
+            {
+            	controller: 'SimpleCtrl',
+            	templateUrl: 'partials/view1.html'
+            })
+	    .when('/view2',
+            {
+            	controller: 'SimpleCtrl',
+            	templateUrl: 'partials/view2.html'
+            })
+	    .otherwise({redirectTo: '/'});
+});
+
 demoApp.controller('SimpleCtrl', function ($scope) {
 	$scope.persons = [
 	  {name: 'vaibhav', city: 'katni'},
